6509fdfb7d40879b1f1ff9e9a639c19742410ccd
wlsdkrla <85570860+wlsdkrla@users.noreply.github.com>
2025-05-27 14:15:55 +0900

    Delete Scripts directory

--- Scripts/Cave_DialogueObjData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Cave_DialogueObjData : MonoBehaviour
-{
-    public int id;
-    public bool isNpc;
-}
